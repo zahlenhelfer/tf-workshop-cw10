41912980ae6025ed821bdd6f7f811bb9a173946c
Marcus Ross <marcus.ross@zahlenhelfer.de>
2023-03-07 11:21:20 +0100

    Security Group hinzugefügt

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ resource "aws_instance" "web" {
   ami           = "ami-0a261c0e5f51090b1"
   user_data     = file("install_webserver.sh")
 }
+
+
+resource "aws_security_group" "web_access" {
+  name        = "web-security-group"
+  description = "Terraform web security group"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
